9de32dc7755f58429685db8a101fbf6d92567f91
Bria <briapaul143@outlook.com>
2022-01-16 17:38:50 -0400

    First Verision on Twitter Clone

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,176 @@
+<template>
+  <q-layout view="lHr lpR fFf">
+    <q-header bordered class="bg-white text-grey-10">
+      <q-toolbar>
+        <q-btn dense flat round icon="menu" @click="toggleLeftDrawer" />
+
+        <q-toolbar-title class="text-weight-bolder"> 
+          <span class="gt-sm"> {{$route.name}} </span>
+          <q-icon name="fas fa-dove" color="primary" size="sm" class="header-icon q-pl-xl lt-md " />
+          </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <!-- left side -->
+    <q-drawer
+      show-if-above
+      v-model="leftDrawerOpen"
+      side="left"
+      bordered
+      :width="200"
+    >
+      <q-icon name="fas fa-dove" color="primary" size="sm" class="q-pl-xl" />
+
+      <q-list class="column items-end">
+        <q-item clickable v-ripple to="/">
+          <q-item-section avatar>
+            <q-icon color="primary" name="house" size="md" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+
+      <q-list class="column items-end">
+        <q-item clickable v-ripple to="/">
+          <q-item-section avatar>
+            <q-icon color="primary" name="explore" size="md" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+
+      <q-list class="column items-end">
+        <q-item clickable v-ripple to="/">
+          <q-item-section avatar>
+            <q-icon color="primary" name="notifications" size="md" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+
+      <q-list class="column items-end">
+        <q-item clickable v-ripple to="/">
+          <q-item-section avatar>
+            <q-icon color="primary" name="mail" size="md" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+
+      <q-list class="column items-end">
+        <q-item clickable v-ripple to="/profile">
+          <q-item-section avatar>
+            <q-icon color="primary" name="person" size="md" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-drawer>
+
+    <!-- right side -->
+    <q-drawer show-if-above v-model="rightDrawerOpen" side="right" bordered>
+      <!-- Search bar -->
+      <q-input
+        color="blue"
+        standout
+        bottom-slots
+        v-model="text"
+        label="Search"
+        counter
+        clearable
+        class="q-ma-md"
+        rounded
+        dense
+      >
+        <template v-slot:prepend>
+          <q-icon name="search" />
+        </template>
+      </q-input>
+
+      <q-list padding>
+        <q-item>
+          <q-item-section>
+            <q-item-label overline class="text-weight-bold"
+              >Trends for you</q-item-label
+            >
+            <q-item-label> U.W.I </q-item-label>
+            <q-item-label caption
+              >New professional promotions at U.W.I
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-separator spaced />
+      </q-list>
+
+      <q-list padding>
+        <q-item>
+          <q-item-section>
+            <q-item-label> U.W.I </q-item-label>
+            <q-item-label caption
+              >New professional promotions at U.W.I
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-separator spaced />
+      </q-list>
+
+      <q-list padding>
+        <q-item>
+          <q-item-section>
+            <q-item-label> U.W.I </q-item-label>
+            <q-item-label caption
+              >New professional promotions at U.W.I
+            </q-item-label>
+          </q-item-section>
+
+          <q-item-section side top>
+            <q-item-label caption>5 min ago</q-item-label>
+          </q-item-section>
+        </q-item>
+
+        <q-separator spaced />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  setup() {
+    const leftDrawerOpen = ref(false);
+    const rightDrawerOpen = ref(false);
+
+    return {
+      leftDrawerOpen,
+      toggleLeftDrawer() {
+        leftDrawerOpen.value = !leftDrawerOpen.value;
+      },
+
+      rightDrawerOpen,
+      toggleRightDrawer() {
+        rightDrawerOpen.value = !rightDrawerOpen.value;
+      },
+    };
+  },
+};
+</script>
+
+<style lang="css">
+.header-icon{
+  position: relative;
+  bottom: 0;
+  left: 40%;
+  transform: translateX(-50%);
+}
+</style>
---
+++ src/pages/HomePage.vue
@@ -0,0 +1,219 @@
+<template>
+  <q-page class="relative-postion">
+    <q-scroll-area class="absolute full-height full-width">
+      <div class="q-py-lg q-px-md row items-end q-col-gutter-md">
+        <div class="col">
+          <q-input
+            bottom-slots
+            v-model="newTwitterContent"
+            placeholder="What's happening?"
+            counter
+            maxlength="280"
+            autogrow
+            class="newTweet"
+          >
+            <template v-slot:before>
+              <q-avatar size="xl">
+                <img
+                  src="https://pbs.twimg.com/media/EMb62bpW4AA43Er?format=jpg&name=small"
+                />
+              </q-avatar>
+            </template>
+          </q-input>
+        </div>
+
+        <div class="col col-shrink">
+          <q-btn
+            @click="addNewTweet"
+            unelevated
+            rounded
+            color="primary"
+            label="Tweet"
+            no-caps
+            :disable="!newTwitterContent"
+            class="q-mb-lg"
+          />
+        </div>
+      </div>
+
+      <q-separator size="3px" color="grey-6" />
+
+      <div class="q-pa-md q-gutter-md">
+        <q-list separator>
+          <transition-group
+            appear
+            enter-active-class="animated fadeIn slow"
+            leave-active-class="animated fadeOut slower"
+          >
+            <q-item v-for="tweet in tweets" :key="tweet.date" class="q-py-md">
+              <q-item-section avatar>
+                <q-avatar top>
+                  <img
+                    src="https://pbs.twimg.com/media/EMb62bpW4AA43Er?format=jpg&name=small"
+                  />
+                </q-avatar>
+              </q-item-section>
+
+              <q-item-section>
+                <q-item-label class="text-subtitle1">
+                  <strong> Cels </strong>
+                  <span class="text-grey-7"
+                    >@cels <br class="lt-md" />
+                    &bull; {{ tweet.date }}</span
+                  >
+                </q-item-label>
+
+                <q-item-label class="tweetContent text-body1">
+                  {{ tweet.content }}
+                </q-item-label>
+
+                <div class="row justify-between q-mt-sm">
+                  <q-btn
+                    flat
+                    round
+                    color="grey"
+                    icon="far fa-comment"
+                    size="sm"
+                  />
+                  <q-btn
+                    flat
+                    round
+                    :color="tweet.retweeted ? 'green' : 'grey'"
+                    icon="fas fa-retweet"
+                    size="sm"
+                    @click="reTweeted(tweet)"
+                  />
+                  <q-btn
+                    flat
+                    round
+                    :color="tweet.liked ? 'pink' : 'grey'"
+                    :icon="tweet.liked ? 'fas fa-heart' : 'favorite_border'"
+                    size="sm"
+                    @click="likeTweet(tweet)"
+                  />
+                  <q-btn
+                    @click="deleteTweet(tweet)"
+                    flat
+                    round
+                    color="grey"
+                    icon="far fa-trash-alt"
+                    size="sm"
+                  />
+                </div>
+              </q-item-section>
+            </q-item>
+
+            <q-separator inset="item" />
+          </transition-group>
+        </q-list>
+      </div>
+    </q-scroll-area>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+import { formatDistance } from "date-fns";
+
+export default defineComponent({
+  name: "HomePage",
+  data() {
+    return {
+      newTwitterContent: "",
+      tweets: [
+        {
+          content:
+            " Can't wait for school to start back! Last semester !! Yay !!!",
+          date: 161165323810,
+          liked: false,
+          retweeted: true,
+        },
+        {
+          content: "Homework: If it goes too easy, you are doing it wrong.",
+          date: 1611653238221,
+          liked: true,
+          retweeted: false,
+        },
+        {
+          content:
+            "That embarrassing moment when you realize, that person wasn't waving at you.",
+          date: 1611653238443,
+          liked: false,
+          retweeted: false,
+        },
+        {
+          content: "I need a 6 month holiday. Twice a year.",
+          date: 1611653238667,
+          liked: false,
+          retweeted: false,
+        },
+        {
+          content:
+            "That awkward moment when somebody is doing dishes, and you slowly put another dish in the sink.",
+          date: 1611653238879,
+          liked: false,
+          retweeted: false,
+        },
+      ],
+    };
+  },
+  methods: {
+    addNewTweet() {
+      let newTweet = {
+        content: this.newTwitterContent,
+        date: Date.now(),
+      };
+      this.tweets.unshift(newTweet);
+      this.newTwitterContent = "";
+    },
+
+    deleteTweet(tweet) {
+      let dateToDelete = tweet.date;
+      let index = this.tweets.findIndex((tweet) => tweet.date === dateToDelete);
+      this.tweets.splice(index, 1);
+    },
+
+    likeTweet(tweet) {
+      let dateOfLikedTweet = tweet.date;
+      let index = this.tweets.findIndex(
+        (tweet) => tweet.date === dateOfLikedTweet
+      );
+
+      if (this.tweets[index].liked == true) {
+        this.tweets[index].liked = false;
+      } else {
+        this.tweets[index].liked = true;
+      }
+    },
+
+    reTweeted(tweet) {
+      let dateOfLikedTweet = tweet.date;
+      let index = this.tweets.findIndex(
+        (tweet) => tweet.date === dateOfLikedTweet
+      );
+
+      if (this.tweets[index].retweeted == true) {
+        this.tweets[index].retweeted = false;
+      } else {
+        this.tweets[index].retweeted = true;
+      }
+    },
+  },
+  filters: {
+    relativeDate(value) {
+      return formatDistance(value, new Date());
+    },
+  },
+});
+</script>
+
+<style lang="css">
+.newTweet {
+  font-size: 19px;
+  line-height: 1.4 !important;
+}
+
+.tweetContent {
+  white-space: pre-line;
+}
+</style>
